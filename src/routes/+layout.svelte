6f3204b4a62cdef4f79ed2ec5b866040989cfd08
Detroyer2222 <49248383+Detroyer2222@users.noreply.github.com>
2024-01-04 22:09:33 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	//Dynamic Theming
+	import 'carbon-components-svelte/css/all.css';
+	import type { LayoutData } from './$types';
+
+	import { Theme, RadioButtonGroup, RadioButton } from 'carbon-components-svelte';
+
+	export let data: LayoutData;
+	let theme = 'g100';
+</script>
+
+<slot />
+
+<Theme
+	bind:theme
+	persist
+	persistKey="__carbon-theme"
+	render="toggle"
+	toggle={{
+		themes: ['g10', 'g100'],
+		labelA: 'Enable dark mode',
+		labelB: 'Enable dark mode',
+		hideLabel: true,
+		size: 'sm'
+	}}
+/>
