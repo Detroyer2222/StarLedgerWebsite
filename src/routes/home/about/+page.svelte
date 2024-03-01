40c115bc4b95ab593ba2d2a267940ed247e375f7
Detroyer2222 <49248383+Detroyer2222@users.noreply.github.com>
2024-03-01 21:03:34 +0100

    fixed about page

--- src/routes/home/about/+page.svelte
+++ src/routes/home/about/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import { type PageData } from './$types';
-
-	export let data: PageData;
 </script>
 
 <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
